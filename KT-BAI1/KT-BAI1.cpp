5171c9b4e16833b8ff6c3a354b43d46eb18d6772
ADMIN <ADMIN@LAB1-MAY06>
2024-03-04 16:47:46 +0700

    Add project files.

---
+++ KT-BAI1/KT-BAI1.cpp
@@ -0,0 +1,17 @@
+﻿// KT-BAI1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//NGUYEN THI PHUOC AN - BÀI CHẴN 
+
+#include <iostream>
+using namespace std;
+int main() {
+	double a, b ,chuVi;
+	cout <<("nhap chieu dai hinh chu nhat:");
+	cin >> a;
+	cout<<("nhap chieu rong hinh chu nhat");
+	cin >> b;
+		chuVi =(a + b) * 2;
+		cout<<("chu vi hinh chu nhat la:") << chuVi << endl;
+	return 0;
+}
+
+
